ac16ff0cef66509f0313a4402676be3051554512
Rainchus <rainmachus@gmail.com>
2023-05-01 16:18:02 -0500

    rels wip, invoke with ./splitdol.py --rels

---
+++ create_rel_ymls.py
@@ -0,0 +1,59 @@
+import os
+import struct
+import sys
+import yaml
+from yaml.representer import SafeRepresenter
+
+class CustomRepresenter(SafeRepresenter):
+    def represent_hex(self, data):
+        return self.represent_scalar('tag:yaml.org,2002:int', '0x{:X}'.format(data))
+
+CustomRepresenter.add_representer(int, CustomRepresenter.represent_hex)
+yaml.add_representer(int, CustomRepresenter.represent_hex)
+
+# Constants
+DOL_FILE = "config/dol.yml"
+START_ADDRESS = 0x805BA9A0
+
+bss_address = 0x803C8460
+
+# Parse command line arguments
+if len(sys.argv) != 2:
+    print("Usage: python script.py <output_directory>")
+    sys.exit(1)
+output_dir = sys.argv[1]
+
+# Create output directory if it doesn't exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Search for rel files in rels/bin directory
+for filename in os.listdir("rels/bin"):
+    if filename.endswith(".rel"):
+        filepath = os.path.join("rels/bin", filename)
+        with open(filepath, "rb") as f:
+            # Read the BSS offset as a 32-bit big-endian integer from offset 0x20
+            f.seek(0x20)
+            bss_offset_bytes = f.read(4)
+            bss_offset = struct.unpack(">I", bss_offset_bytes)[0]
+
+            # Calculate the BSS address based on the offset and the previous BSS address
+            bss_address += bss_offset
+
+            # Generate the output dictionary
+            size = os.path.getsize(filepath)
+            rel_name = os.path.splitext(filename)[0]
+            output_dict = {
+                "path": filepath,
+                "bss_address": bss_address,
+                "address": START_ADDRESS,
+                "dol": DOL_FILE
+            }
+
+            # Write the output dictionary to a YAML file in the output directory with the same name as the rel
+            output_filename = os.path.join(output_dir, rel_name + ".yml")
+            with open(output_filename, "w") as outfile:
+                yaml.dump(output_dict, outfile, default_flow_style=False, sort_keys=False, allow_unicode=True)
+
+            # Update start address for next rel file
+            START_ADDRESS += size
--- splitdol.py
+++ splitdol.py
@@ -2,19 +2,32 @@
 
 import subprocess
 import os
+import argparse
 
 # Create 'asm/' directory if it doesn't exist
 if not os.path.exists("asm"):
     os.makedirs("asm")
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--rels", action="store_true", help="Disassemble rels")
+args = parser.parse_args()
 
-# Run analyser.py
-subprocess.run(["python3", "./tools/ppcdis/analyser.py", "config/dol.yml", "asm/labels_out.txt", "asm/relocs_out.txt"])
+us_rels = ["config/aaa.yml", "config/aji.yml", "config/bom.yml", "config/dmo.yml", "config/dou.yml", "config/eki.yml", "config/end.yml", "config/gon.yml", "config/gor.yml", "config/gra.yml", "config/hei.yml", "config/hom.yml", "config/jin.yml", "config/jon.yml", "config/kpa.yml", "config/las.yml", "config/moo.yml", "config/mri.yml", "config/muj.yml",
+"config/nok.yml", "config/pik.yml", "config/rsh.yml", "config/sys.yml", "config/tik.yml", "config/tou.yml", "config/tou2.yml", "config/usu.yml", "config/win.yml", "config/yuu.yml"]
 
-# Run csv_to_yaml_symbols.py
-subprocess.run(["python3", "./tools/csv_to_yaml_symbols.py"])
+print('Running relextern.py...')
+subprocess.run(["python3", "./tools/ppcdis/relextern.py", "asm/relextern_out_file", *us_rels])
+#if --rels arg is found
+if args.rels:
+    print("Running analyser.py...")
+    subprocess.run(["python3", "./tools/ppcdis/analyser.py", "config/aaa.yml", "asm/label_aaa_out.txt", "asm/relocs_aaa_out.txt", "-l", "asm/relextern_out_file"])
+else:
+    # Run analyser.py
+    print("Running analyser.py...")
+    subprocess.run(["python3", "./tools/ppcdis/analyser.py", "-l" "asm/relextern_out_file", "config/dol.yml", "asm/labels_out.txt", "asm/relocs_out.txt"])
 
+    # Run csv_to_yaml_symbols.py
+    subprocess.run(["python3", "./tools/csv_to_yaml_symbols.py"])
 
-
-# Run disassembler.py
-subprocess.run(["python3", "./tools/ppcdis/disassembler.py", "-m", "config/symbols.yml", "config/dol.yml", "asm/labels_out.txt", "asm/relocs_out.txt", "asm/disasm.s"])
+    # Run disassembler.py
+    subprocess.run(["python3", "./tools/ppcdis/disassembler.py", "-m", "config/symbols.yml", "config/dol.yml", "asm/labels_out.txt", "asm/relocs_out.txt", "asm/disasm.s"])

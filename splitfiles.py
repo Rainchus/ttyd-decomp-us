075d5644e6109dedd3c2cceab6fd3434cae4a63a
Rainchus <rainmachus@gmail.com>
2023-07-04 16:08:00 -0500

    update build system, match rels up to dmo

--- splitfiles.py
+++ splitfiles.py
@@ -4,6 +4,14 @@ import subprocess
 import os
 import argparse
 
+# Color codes
+class Colors:
+    RESET = '\033[0m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+
 # Create 'asm/' directory if it doesn't exist
 if not os.path.exists("asm"):
     os.makedirs("asm")
@@ -11,6 +19,15 @@ if not os.path.exists("asm"):
 # Create 'asm/aaa' directory if it doesn't exist
 if not os.path.exists("asm/aaa"):
     os.makedirs("asm/aaa")
+
+if not os.path.exists("asm/aji"):
+    os.makedirs("asm/aji")
+
+if not os.path.exists("asm/bom"):
+    os.makedirs("asm/bom")
+
+if not os.path.exists("asm/dmo"):
+    os.makedirs("asm/dmo")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--rels", action="store_true", help="Disassemble rels")
@@ -20,12 +37,25 @@ us_rels = ["config/aaa.yml", "config/aji.yml", "config/bom.yml", "config/dmo.yml
 "config/nok.yml", "config/pik.yml", "config/rsh.yml", "config/sys.yml", "config/tik.yml", "config/tou.yml", "config/tou2.yml", "config/usu.yml", "config/win.yml", "config/yuu.yml"]
 
 # Run relextern.py
-print('Running relextern.py...')
+print(Colors.GREEN + 'Running relextern.py...' + Colors.RESET)
 subprocess.run(["python3", "./tools/ppcdis/relextern.py", "asm/relextern_out_file", *us_rels])
+
 
 # Run analyser.py on aaa.yml
 print("Running analyser.py...")
 subprocess.run(["python3", "./tools/ppcdis/analyser.py", "config/aaa.yml", "asm/aaa/label_aaa_out.txt", "asm/aaa/relocs_aaa_out.txt", "-l", "asm/relextern_out_file"])
+
+# Run analyser.py on aji.yml
+print("Running analyser.py...")
+subprocess.run(["python3", "./tools/ppcdis/analyser.py", "config/aji.yml", "asm/aji/label_aji_out.txt", "asm/aji/relocs_aji_out.txt", "-l", "asm/relextern_out_file"])
+
+# Run analyser.py on bom.yml
+print("Running analyser.py...")
+subprocess.run(["python3", "./tools/ppcdis/analyser.py", "config/bom.yml", "asm/bom/label_bom_out.txt", "asm/bom/relocs_bom_out.txt", "-l", "asm/relextern_out_file"])
+
+# Run analyser.py on dmo.yml
+print("Running analyser.py...")
+subprocess.run(["python3", "./tools/ppcdis/analyser.py", "config/dmo.yml", "asm/dmo/label_dmo_out.txt", "asm/dmo/relocs_dmo_out.txt", "-l", "asm/relextern_out_file"])
 
 # Run analyser.py on dol.yml
 print("Running analyser.py...")
@@ -39,5 +69,15 @@ subprocess.run(["python3", "./tools/csv_to_yaml_symbols.py"])
 # Run disassembler.py on dol
 subprocess.run(["python3", "./tools/ppcdis/disassembler.py", "-m", "config/symbols.yml", "config/dol.yml", "asm/labels_out.txt", "asm/relocs_out.txt", "asm/disasm.s"])
 
+
 # Run disassembler.py on aaa.rel
-subprocess.run(["python3", "./tools/ppcdis/disassembler.py", "-m", "config/symbols.yml", "config/aaa.yml", "asm/aaa/label_aaa_out.txt", "asm/aaa/relocs_aaa_out.txt", "asm/aaa/aaa.s"])
+subprocess.run(["python3", "./tools/ppcdis/disassembler.py", "-m", "config/symbols.yml", "-o", "config/disasm_overrides.yml", "config/aaa.yml", "asm/aaa/label_aaa_out.txt", "asm/aaa/relocs_aaa_out.txt", "asm/aaa/_aaa.s"])
+
+# Run disassembler.py on aji.rel
+subprocess.run(["python3", "./tools/ppcdis/disassembler.py", "-m", "config/symbols.yml", "-o", "config/disasm_overrides.yml", "config/aji.yml", "asm/aji/label_aji_out.txt", "asm/aji/relocs_aji_out.txt", "asm/aji/_aji.s"])
+
+# Run disassembler.py on bom.rel
+subprocess.run(["python3", "./tools/ppcdis/disassembler.py", "-m", "config/symbols.yml", "-o", "config/disasm_overrides.yml", "config/bom.yml", "asm/bom/label_bom_out.txt", "asm/bom/relocs_bom_out.txt", "asm/bom/_bom.s"])
+
+# Run disassembler.py on dmo.rel
+subprocess.run(["python3", "./tools/ppcdis/disassembler.py", "-m", "config/symbols.yml", "-o", "config/disasm_overrides.yml", "config/dmo.yml", "asm/dmo/label_dmo_out.txt", "asm/dmo/relocs_dmo_out.txt", "asm/dmo/_dmo.s"])
--- tools/configure.py
+++ tools/configure.py
@@ -1,25 +1,37 @@
-import ninja_syntax
 import os
-import sys
+import ninja_syntax
+
+
+rel_folder_paths = {
+    "aaa": "asm/aaa",
+    "aji": "asm/aji",
+    "bom": "asm/bom",
+    "dmo": "asm/dmo"
+}
+
+file_lists = {
+    "aaa": {"c": [], "s": [], "o": []},
+    "aji": {"c": [], "s": [], "o": []},
+    "bom": {"c": [], "s": [], "o": []},
+    "dmo": {"c": [], "s": [], "o": []}
+}
 
 dir_path = 'src/'
 asm_path = 'asm/'
-rels_path = 'rels/'
 
-aaa_folder_path = 'asm/aaa/'
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[0m'
 
-#if DEVKITPPC isn't found, throw an error
-if os.getenv('DEVKITPPC') is None:
-    print('DEVKITPPC environment variable not set')
-    sys.exit()
 
-#if wibo isn't found, throw an error
-if os.getenv('WIBO') is None:
-    print('WIBO environment variable not set')
-    print('Set by installing the binary and then creating an environment variable')
-    print('named WIBO with a path to the wibo binary')
-    print('https://github.com/decompals/wibo/releases')
-    sys.exit()
+def append_extension(filename, extension='.o'):
+    return filename + extension
 
 c_files = []
 for root, dirs, files in os.walk(dir_path):
@@ -35,133 +47,118 @@ for file in os.listdir(asm_path):
     if os.path.isfile(file_path) and file.endswith('.s'):
         s_files.append(file_path)
 
-aaa_c_files = []
-for root, dirs, files in os.walk(aaa_folder_path):
-    if 'rels/bin' in root: #skip rel binaries
-        continue  # skip this directory
-    for file in files:
-        if file.endswith('.c'):
-            aaa_c_files.append(os.path.join(root, file))
-
-aaa_s_files = []
-for root, dirs, files in os.walk(aaa_folder_path):
-    if 'rels/bin' in root: #skip rel binaries
-        continue  # skip this directory
-    for file in files:
-        if file.endswith('.s'):
-            aaa_s_files.append(os.path.join(root, file))
-
-def append_extension(filename, extension='.o'):
-    return filename + extension
-
-def append_prefix(filename, extension):
-    return extension + filename
-
 # Combine the lists and change file extensions
 o_files = []
 for file in c_files + s_files:
     o_files.append("build/" + append_extension(file))
 
-# Combine the lists and change file extensions
-aaa_o_files = []
-for file in aaa_c_files + aaa_s_files:
-    aaa_o_files.append("build/" + append_extension(file))
+ninja_file = ninja_syntax.Writer(open('build.ninja', 'w'))
 
-header = (
-    "AS = $$DEVKITPPC/bin/powerpc-eabi-as\n"
-    "CPP = $$DEVKITPPC/bin/powerpc-eabi-cpp -P\n"
-    "BUILD_DIR = build\n"
-    "DOL = $BUILD_DIR/main.dol\n"
-    "ELF = $BUILD_DIR/ttyd_us.elf\n"
-    "MAP = $BUILD_DIR/ttyd_us.map\n"
-    "LDSCRIPT_DOL = ldscript.lcf\n"
-    "LDSCRIPT_REL = config/partial.lcf\n"
-    "OPTFLAGS = -O4,p\n"
-    "DOL = $BUILD_DIR/main.dol\n"
-    "MWCC_VERSION = GC/1.3.2\n"
-    "MWLD_VERSION = GC/1.3.2\n"
-    "CC = $$WIBO tools/mwcc_compiler/$MWCC_VERSION/mwcceppc.exe\n"
-    "LD = $$WIBO tools/mwcc_compiler/$MWLD_VERSION/mwldeppc.exe\n"
-    "ELF2DOL = ./tools/ppcdis/elf2dol.py\n"
-    "ELF2REL = tools/elf2rel\n"
-    "SHA1SUM = sha1sum\n"
-    "INCLUDES = -i include/\n"
-    "ASM_INCLUDES = -I include/\n"
-    "ASFLAGS = -mgekko $ASM_INCLUDES\n"
-    "MAPGEN = -map $MAP\n"
-    "LDFLAGS = $MAPGEN -fp hard -nodefaults\n"
-    "CFLAGS = -Cpp_exceptions off -proc gekko -fp hard $OPTFLAGS -nodefaults -sdata 48 -sdata2 8 -inline all,deferred -use_lmw_stmw on -enum int -rostr $INCLUDES\n"
-    "NAME = ttyd_us\n"
-    #"LD_REL = $LD -lcf partial.lcf -nodefaults -fp hard -r1 -m _prolog -g $in $MAPGEN -o $out\n"
-)
-
-# Create a Ninja build file object
-with open('build.ninja', 'w') as file:
-    file.write(f'{header}')
-ninja_file = ninja_syntax.Writer(open('build.ninja', 'a'))
-
-# Define a new rule
-ninja_file.rule('gen_ldscript',
-                 command = "$CPP -MMD -MP -MT $out -MF $out.d $ASM_INCLUDES -I . -DBUILD_DIR=$BUILD_DIR -o $out $in",
-                 description = "Generating linker script $out from $in",
-                 depfile = "$out.d",
-                 deps = "msvc")
+ninja_file.variable('AS', '$$DEVKITPPC/bin/powerpc-eabi-as')
+ninja_file.variable('CPP', '$$DEVKITPPC/bin/powerpc-eabi-cpp -P')
+ninja_file.variable('BUILD_DIR', 'build')
+ninja_file.variable('DOL', '$BUILD_DIR/main.dol')
+ninja_file.variable('ELF', '$BUILD_DIR/ttyd_us.elf')
+ninja_file.variable('MAP', '$BUILD_DIR/ttyd_us.map')
+ninja_file.variable('LDSCRIPT_DOL', 'ldscript.lcf')
+ninja_file.variable('LDSCRIPT_REL', 'config/partial.lcf')
+ninja_file.variable('OPTFLAGS', '-O4,p')
+ninja_file.variable('MWCC_VERSION', 'GC/1.3.2')
+ninja_file.variable('MWLD_VERSION', 'GC/1.3.2')
+ninja_file.variable('CC', '$$WIBO tools/mwcc_compiler/$MWCC_VERSION/mwcceppc.exe')
+ninja_file.variable('LD', '$$WIBO tools/mwcc_compiler/$MWLD_VERSION/mwldeppc.exe')
+ninja_file.variable('ELF2DOL', './tools/ppcdis/elf2dol.py')
+ninja_file.variable('ELF2REL', './tools/ppcdis/elf2rel.py')
+ninja_file.variable('SHA1SUM', 'sha1sum')
+ninja_file.variable('INCLUDES', '-i include/')
+ninja_file.variable('ASM_INCLUDES', '-I include/')
+ninja_file.variable('ASFLAGS', '-mgekko $ASM_INCLUDES')
+ninja_file.variable('MAPGEN', '-map $MAP')
+ninja_file.variable('LDFLAGS', '$MAPGEN -fp hard -nodefaults')
+ninja_file.variable('CFLAGS', '-Cpp_exceptions off -proc gekko -fp hard $OPTFLAGS -nodefaults -sdata 48 -sdata2 8 -inline all,deferred -use_lmw_stmw on -enum int -rostr $INCLUDES')
 
 ninja_file.rule('c_files',
                  command = "$CC $CFLAGS -c -o $in $out",
-                 description = "Compiling .c file",
-                 deps = "msvc")
+                 description = MAGENTA + "Compiling .c file" + RESET,
+                 depfile = "$out.d",
+                 deps = "gcc")
 
 ninja_file.rule('s_files',
                  command = "$AS $ASFLAGS -o $out $in",
-                 description = "Assembling .s file",
-                 deps = "msvc")
+                 description = MAGENTA + "Assembling .s file" + RESET)
+
+ninja_file.rule('aaa_make_elf',
+                 command = "($LD -lcf partial.lcf -nodefaults -fp hard -r1 -m _prolog -g $in -o $out)")
+
+ninja_file.rule('aji_make_elf',
+                 command = "($LD -lcf partial.lcf -nodefaults -fp hard -r1 -m _prolog -g $in -o $out)")
+
+ninja_file.rule('bom_make_elf',
+                 command = "($LD -lcf partial.lcf -nodefaults -fp hard -r1 -m _prolog -g $in -o $out)")
+
+ninja_file.rule('dmo_make_elf',
+                 command = "($LD -lcf partial.lcf -nodefaults -fp hard -r1 -m _prolog -g $in -o $out)")
+
+ninja_file.rule('aaa_elf_to_rel',
+                command="(python3 ./tools/ppcdis/elf2rel.py -n 15 -m 1 -v 3 --name-size 2D $in build/ttyd_us.elf) && (result=$$($SHA1SUM -c sha1/aaa.rel.sha1); if [ \"$${result#* }\" = \"OK\" ]; then echo \"" + GREEN + "$${result}\"" + RESET + "; else echo \"" + RED + "$${result}\"" + RESET + "; fi)",
+                description = "Creating aaa.rel...")
+
+ninja_file.rule('aji_elf_to_rel',
+                command="(python3 ./tools/ppcdis/elf2rel.py -n 15 -m 2 -v 3 --name-offset 2D --name-size 2D $in build/ttyd_us.elf) && (result=$$($SHA1SUM -c sha1/aji.rel.sha1); if [ \"$${result#* }\" = \"OK\" ]; then echo \"" + GREEN + "$${result}\"" + RESET + "; else echo \"" + RED + "$${result}\"" + RESET + "; fi)",
+                description = "Creating aji.rel...")
+
+ninja_file.rule('bom_elf_to_rel',
+                command="(python3 ./tools/ppcdis/elf2rel.py -n 15 -m 3 -v 3 --name-offset 5A --name-size 2D $in build/ttyd_us.elf) && (result=$$($SHA1SUM -c sha1/bom.rel.sha1); if [ \"$${result#* }\" = \"OK\" ]; then echo \"" + GREEN + "$${result}\"" + RESET + "; else echo \"" + RED + "$${result}\"" + RESET + "; fi)",
+                description = "Creating bom.rel...")
+
+ninja_file.rule('dmo_elf_to_rel',
+                command="(python3 ./tools/ppcdis/elf2rel.py -n 15 -m 4 -v 3 --name-offset 87 --name-size 2D $in build/ttyd_us.elf) && (result=$$($SHA1SUM -c sha1/dmo.rel.sha1); if [ \"$${result#* }\" = \"OK\" ]; then echo \"" + GREEN + "$${result}\"" + RESET + "; else echo \"" + RED + "$${result}\"" + RESET + "; fi)",
+                description = "Creating dmo.rel...")
 
 ninja_file.rule('make_elf',
                  command = "$LD $LDFLAGS -o $out -lcf $LDSCRIPT_DOL $in",
-                 description = ".o Files to ELF",
-                 deps = "msvc")
+                 description = "Creating ttyd_us.elf...")
 
 ninja_file.rule('make_dol',
-                command = "(python3 $ELF2DOL $in -o $out) && ($SHA1SUM -c sha1/ttyd.us.sha1)",
-                description = "Converting ELF to DOL",
-                deps = "msvc")
-        
-ninja_file.rule('make_rel_elf',
-                 command = "($LD -lcf partial.lcf -nodefaults -fp hard -r1 -m _prolog -g $in $MAPGEN -o $out)",
-                 description = "Creating elf of rel",
-                 deps = "msvc")
+                command="(python3 $ELF2DOL $in -o $out) && (result=$$($SHA1SUM -c sha1/ttyd.us.sha1); if [ \"$${result#* }\" = \"OK\" ]; then echo \"" + GREEN + "$${result}\"" + RESET + "; else echo \"" + RED + "$${result}\"" + RESET + "; fi)",
+                description= "Creating ttyd_us.dol...")
 
-ninja_file.rule('aaa_elf_to_rel',
-                 command = "(python3 ./tools/ppcdis/elf2rel.py -n 15 -m 1 -v 3 --name-size 2D $in build/ttyd_us.elf) && ($SHA1SUM -c sha1/aaa.rel.sha1)",
-                 description = "aaa.elf.rel building",
-                 deps = "msvc")
 
-ninja_file.rule('aji_elf_to_rel',
-                 command = "(python3 ./tools/ppcdis/elf2rel.py -n 15 -m 2 -v 3 --name-size 2D $in build/ttyd_us.elf) && ($SHA1SUM -c sha1/aji.rel.sha1)",
-                 description = "Checkum check",
-                 deps = "msvc")
+ninja_file.build("build/ttyd_us.elf", "make_elf", o_files)
+ninja_file.build("build/ttyd_us.dol", "make_dol", "build/ttyd_us.elf")
 
-for c_file in c_files:
-    ninja_file.build("build/" + append_extension(c_file), "c_files", c_file)
-for s_file in s_files:
-    ninja_file.build("build/" + append_extension(s_file), "s_files", s_file)
+# Common dependency for all .elf files
+ninja_file.build("build/common_dep", "phony", inputs=["build/ttyd_us.elf"])
 
-ninja_file.build("build/ttyd_us.elf", "make_elf ", o_files)
-ninja_file.build("build/ttyd_us.dol", "make_dol ", "build/ttyd_us.elf")
+# Iterate over the directory paths
+for key, folder_path in rel_folder_paths.items():
+    # Iterate over the files in the source folder
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            # Check if the file has the desired extensions
+            if file.endswith(".c"):
+                # Add the file to the 'c' list for the current key
+                file_lists[key]["c"].append(file)
+                file_lists[key]["o"].append(file + ".o")
+                ninja_file.build(f"build/{key}/{file}.o", "c_files", f"{folder_path}/{file}")
+            elif file.endswith(".s"):
+                # Add the file to the 's' list for the current key
+                file_lists[key]["s"].append(file)
+                file_lists[key]["o"].append(file + ".o")
+                ninja_file.build(f"build/{key}/{file}.o", "s_files", f"{folder_path}/{file}")
+    ninja_file.build(f"build/{key}/{key}.elf", f"{key}_make_elf", [f"build/{key}/{file}" for file in file_lists[key]["o"]], implicit=["build/common_dep"])
 
-for aaa_c_file in aaa_c_files:
-    ninja_file.build("build/" + append_extension(aaa_c_file), "c_files", aaa_c_file)
-for aaa_s_file in aaa_s_files:
-    ninja_file.build("build/" + append_extension(aaa_s_file), "s_files", aaa_s_file)
+    ninja_file.build(f"build/{key}/{key}.rel", f"{key}_elf_to_rel", f"build/{key}/{key}.elf")
 
-print(aaa_s_files)
 
-ninja_file.build("build/rels/aaa/aaa.elf", "make_rel_elf",  aaa_o_files, "build/ttyd_us.elf")
-ninja_file.build("build/rels/aaa/aaa.rel.ok", "aaa_elf_to_rel",  "build/rels/aaa/aaa.elf")
-
-# ninja_file.build("build/rels/aji/aji.rel", "make_rel_elf",  aji_o_files, "build/ttyd_us.elf")
-# ninja_file.build("build build/rels/aji/aji.rel.ok", "aji_elf_to_rel",  "build/rels/aji/aji.rel")
-
+# Print all the files for each key
+# for key, extensions in file_lists.items():
+#     print(f"Files for key '{key}':")
+#     for ext, files in extensions.items():
+#         print(f"Extension '{ext}':")
+#         for file in files:
+#             print(file)
+#     print()
+    
 print ("build.ninja generated")
 ninja_file.close()
